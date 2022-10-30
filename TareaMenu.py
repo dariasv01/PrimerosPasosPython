d3ed34ec3be98d3686fcf583a94befc3f3328653
David Arias <dariasv01@gmail.com>
2022-10-30 14:23:51 +0100

    Agenda

--- TareaMenu.py
+++ TareaMenu.py
@@ -20,18 +20,19 @@ def addTelephone(nombre):
                 agendaMostrar()
             case other:
                 addTelephone(nombre)
-def borrarContacto():
+def deleteContac():
     nombre = input("Introduce el nombre del contacto: ")
     if(agenda.__contains__(nombre)):
         agenda.pop(nombre)
     else:
         selector = input(f"El nombre {nombre} no existe\nPulse 1 para eliminar otro contacto o pulse cualquier letra para salir")
-        if(selector=="1"):
-            borrarContacto()
-        else:
-            mostrarAgenda()
+        match selector:
+            case '1':
+                deleteContac()
+            case other:
+                mostrarAgenda()
 
-def anadirContacto():
+def addContac():
     nombre = input("Introduce el nombre del contacto: ")
     addTelephone(nombre)
 
@@ -41,10 +42,10 @@ def agendaMostrar():
 
     match command:
         case '3':
-            borrarContacto()
+            deleteContac()
             agendaMostrar()
         case '2':
-            anadirContacto()
+            addContac()
             agendaMostrar()
         case '1':
             mostrarAgenda()
