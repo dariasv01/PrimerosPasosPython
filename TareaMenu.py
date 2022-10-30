6ddbed8774f30205576251ce8fa82cd58506954f
David Arias <dariasv01@gmail.com>
2022-10-30 13:50:51 +0100

    Agenda

--- TareaMenu.py
+++ TareaMenu.py
@@ -1,11 +1,25 @@
 import re
 
+
 agenda = {}
 
 def mostrarAgenda():
+    print("---Agenda---")
     for k, v in agenda.items():
-        print(f"{k}:{v}\n")
+        print(f"Contacto {k} - Teléfono {v}\n")
 
+def addTelephone(nombre):
+    tlfn = input("Introduce el teléfono XXXXXXXXX: ")
+    if (re.findall(r"[\d]{9}", tlfn)):
+        agenda[nombre] = tlfn
+    else:
+        print(f"Número incorrecto")
+        command = input("1 Para volver al menú\nPara volver a añadir un número pulse otro\n")
+        match command:
+            case '1':
+                agendaMostrar()
+            case other:
+                addTelephone(nombre)
 def borrarContacto():
     nombre = input("Introduce el nombre del contacto: ")
     if(agenda.__contains__(nombre)):
@@ -19,16 +33,11 @@ def borrarContacto():
 
 def anadirContacto():
     nombre = input("Introduce el nombre del contacto: ")
+    addTelephone(nombre)
 
-    tlfn = input("Introduce el teléfono XXX-XXX-XXX: ")
-    if(re.findall(r"[\d]{3}-[\d]{3}-[\d]{3}", tlfn)):
-       agenda[nombre] = tlfn
-    else:
-       print(f"Número incorrecto")
-       anadirContacto()
 
 def agendaMostrar():
-    command = input("1 para mostrar agenda\n2 Para añadir contacto\n3 Para eliminar contacto\n")
+    command = input("1 para mostrar agenda\n2 Para añadir contacto\n3 Para eliminar contacto\n0 Para salir\n")
 
     match command:
         case '3':
@@ -40,6 +49,8 @@ def agendaMostrar():
         case '1':
             mostrarAgenda()
             agendaMostrar()
+        case '0':
+            exit()
         case other:
              agendaMostrar()
 
